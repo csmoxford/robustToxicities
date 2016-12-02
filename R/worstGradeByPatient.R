3169059c83ce49e4d8ec5aa9f6a21c33496d7dcc
finite2 <dutton.peter@gmail.com>
2016-12-02 10:12:32 +0000

    table_tox_categories
    Added function table_tox_categories which is a table_values wrapper
    (prettyTables)

--- R/worstGradeByPatient.R
+++ R/worstGradeByPatient.R
@@ -10,6 +10,8 @@
 #' @details
 #' worstGradeByPatientSystem does the same thing but wraps over all categories.
 #'
+#' @return Returns a data.frame
+#'
 #' @export worstGradeByPatient
 
 
@@ -69,4 +71,85 @@ worstGradeByPatientSystem = function(toxDB, df = NULL){
   }
 
   return(df)
+}
+
+
+#' Generate a prettyTable of worst toxicity by category.
+#'
+#' This is a wrapper for the table_values function in prettyTables for worst grade by system.
+#'
+#' @param data A data.frame This should be the output from \code{\link{worstGradeByPatientSystem}}
+#' @param categoryList A list of categories. Default is all. A subset is selected by changing this value.
+#' @param type A vector of the method to summarise the var
+#' @param strata A string of the column to used to stratify on
+#' @param strata.names Otional. A named vector of alternative strata names
+#' @param strata.count TRUE/FALSE for displaying strata counts at the top of each column
+#' @param overall TRUE/FALSE for including an overall column
+#' @param count "n","miss" or "none" providing the counts, missing values or omitting for each column for numeric variables
+#' @param round A value or vector for the number of significant figures to report the data to
+#'
+#' categoryList can be replaced by other column names if the standard categorisation is not being used. For more flexibility you can view the code by typing table_tox_categories.
+#'
+#' Available methods and values for \strong{Type}:
+#' \tabular{cc}{ "miqr" \tab median (Q25,Q75) \cr "miqrr" \tab median (Q25,Q75)[min,max] \cr "mrng" \tab median (Q0,Q100) \cr "avsd" \tab mean (sd) \cr "avci" \tab mean (confidence interval) \cr "st" \tab count \cr "str" \tab count/total \cr "stp" \tab count (percent) \cr "strp" \tab count/total (percent)
+#' }
+#' @return Returns a data.frame
+#' @export table_tox_categories
+#' @importFrom prettyTables table_values
+table_tox_categories = function(data, categoryList = "all", strata = NULL, strata.names = NULL, strata.count = TRUE, overall = TRUE, count = "n", round = 3){
+
+  if(categoryList == "all"){
+    categoryList = c(
+      "Blood and lymphatic system disorders",
+      "Cardiac disorders",
+      "Congenital, familial and genetic disorders",
+      "Ear and labyrinth disorders",
+      "Endocrine disorders",
+      "Eye disorders",
+      "Gastrointestinal disorders",
+      "General disorders and administration site conditions",
+      "Hepatobiliary disorders",
+      "Immune system disorders",
+      "Infections and infestations",
+      "Injury, poisoning and procedural complications",
+      "Investigations",
+      "Metabolism and nutrition disorders",
+      "Musculoskeletal and connective tissue disorders",
+      "Neoplasms benign, malignant and unspecified (incl cysts and polyps)",
+      "Nervous system disorders",
+      "Pregnancy, puerperium and perinatal conditions",
+      "Psychiatric disorders",
+      "Renal and urinary disorders",
+      "Reproductive system and breast disorders",
+      "Respiratory, thoracic and mediastinal disorders",
+      "Skin and subcutaneous tissue disorders",
+      "Social circumstances",
+      "Surgical and medical procedures",
+      "Vascular disorders",
+      "Other",
+      "Missing"
+    )
+  }
+
+  var.order = list()
+  for(i in categoryList){
+    var.order[[i]] = c(0,1,2,3,4,5)
+  }
+
+
+  tble = table_values(
+    data,
+    var = categoryList,
+    var.names = categoryList,
+    var.order = var.order,
+    strata = strata,
+    strata.names = strata.names,
+    strata.count = strata.count,
+    type = rep("stp",28),
+    round = 1,
+    overall = overall,
+    count = count
+  )
+
+  names(tble)[1:2] = c("Category", "Grade")
 }
